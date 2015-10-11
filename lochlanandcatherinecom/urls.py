123d986a081595a2159c2517a08c2cf6c95566c2
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2015-10-10 23:52:23 -0700

    Add event page, change external links, add minor supporting styles

--- lochlanandcatherinecom/urls.py
+++ lochlanandcatherinecom/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
+    url(r'^event/$', TemplateView.as_view(template_name="event.html"), name='event'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
