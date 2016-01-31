148752fce7d02c70c39cea14256ac8184e114bdd
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2016-01-31 13:30:01 -0800

    Add registry page

--- lochlanandcatherinecom/urls.py
+++ lochlanandcatherinecom/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name='about'),
     url(r'^event/$', TemplateView.as_view(template_name="event.html"), name='event'),
+    url(r'^registry/$', TemplateView.as_view(template_name="registry.html"), name='registry'),
     url(r'^rsvp/(?P<pk>[0-9]+)/$', GuestRsvpView.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
